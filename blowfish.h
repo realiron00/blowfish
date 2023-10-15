eea5ee5c103adf63cd8e65470ea436373d5b1b5a
realiron00 <ic1595@naver.com>
2023-10-15 17:58:26 +0900

    10.15 1758

--- blowfish.h
+++ blowfish.h
@@ -1,8 +1,15 @@
+//Blowfish.h
+
 typedef struct {
-  unsigned long P[16 + 2];
-  unsigned long S[4][256];
+  unsigned long P[16 + 2]; //초기화 및 라운드키 저장
+  unsigned long S[4][256]; //S-box
 } BF;
 
+//라운드키 생성 함수
 void Blowfish_Init(BF *ctx, unsigned char *key, int keyLen);
-void Blowfish_Encrypt(BF *ctx, unsigned long *xl, unsigned long *xr);
-void Blowfish_Decrypt(BF *ctx, unsigned long *xl, unsigned long *xr);
+
+//암호화 함수
+void Blowfish_Enc(BF *ctx, unsigned long *xl, unsigned long *xr);
+
+//복호화 함수
+void Blowfish_Dec(BF *ctx, unsigned long *xl, unsigned long *xr);
